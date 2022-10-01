06327e70942965056e7af9c0a859af23ec05b15f
Broke Machine <emile.ettel@epitech.eu>
2022-10-01 17:30:12 +0200

    Moved stack push to better place

--- udpServer.cpp
+++ udpServer.cpp
@@ -9,45 +9,31 @@
 
 using boost::asio::ip::udp;
 
-// std::string make_daytime_string() {
-//     using namespace std;  // For time_t, time and ctime;
-//     time_t now = time(0);
-//     return ctime(&now);
-// }
-
-class UdpServer {
-   public:
-    UdpServer(boost::asio::io_context& io_context)
-        : _socket(io_context, udp::endpoint(udp::v4(), 8000)) {
+class UdpServer
+{
+public:
+    UdpServer(boost::asio::io_context &io_context)
+        : _socket(io_context, udp::endpoint(udp::v4(), 8000))
+    {
         start_receive();
     }
 
-   private:
-    void start_receive() {
+private:
+    void start_receive()
+    {
         std::cout << "Start receiving\n";
         _socket.async_receive_from(
             boost::asio::buffer(_recv_buffer), _remote_endpoint,
             boost::bind(&UdpServer::handle_receive, this,
                         boost::asio::placeholders::error,
                         boost::asio::placeholders::bytes_transferred));
-        if (_recv_buffer[0] != 64)
-            return;
-        _stack_recv.push(_recv_buffer.data());
-        std::cout << _recv_buffer.data() << std::endl;
-        std::cout << "Stack :";
-        for (int i = 0; i < _stack_recv.size(); i++) {
-            std::cout << _stack_recv.top();
-            std::cout << " ";
-        }
-        std::cout << std::endl;
-        }
+    }
 
-    void handle_receive(const boost::system::error_code& error,
-                        std::size_t /*bytes_transferred*/) {
-        if (!error) {
-            // boost::shared_ptr<std::string> message(
-            // new std::string(make_daytime_string()));
-            // std::cout << "Sending " << *message << std::endl;
+    void handle_receive(const boost::system::error_code &error,
+                        std::size_t /*bytes_transferred*/)
+    {
+        if (!error)
+        {
             boost::shared_ptr<std::string> message(
                 new std::string("Hello from server"));
             _socket.async_send_to(boost::asio::buffer(*message), _remote_endpoint,
@@ -56,12 +42,22 @@ class UdpServer {
                                               boost::asio::placeholders::bytes_transferred));
 
             start_receive();
+            _stack_recv.push(_recv_buffer.data());
+            std::cout << _recv_buffer.data() << std::endl;
+            std::cout << "Stack :";
+            for (int i = 0; i < _stack_recv.size(); i++)
+            {
+                std::cout << _stack_recv.top();
+                std::cout << " ";
+            }
+            std::cout << std::endl;
         }
     }
 
     void handle_send(boost::shared_ptr<std::string> /*message*/,
-                     const boost::system::error_code& /*error*/,
-                     std::size_t /*bytes_transferred*/) {
+                     const boost::system::error_code & /*error*/,
+                     std::size_t /*bytes_transferred*/)
+    {
     }
 
     udp::socket _socket;
@@ -70,12 +66,16 @@ class UdpServer {
     std::stack<std::string> _stack_recv;
 };
 
-int main() {
-    try {
+int main()
+{
+    try
+    {
         boost::asio::io_context io_context;
         UdpServer server(io_context);
         io_context.run();
-    } catch (std::exception& e) {
+    }
+    catch (std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
     }
 
